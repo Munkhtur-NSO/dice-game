cafcc2a17c6477ef372dfa2d43bc985d8a225e0d
munkhuu <Munkhtur@nso.mn>
2022-12-18 18:33:18 +0800

    Toglogchiin eeljiig solidog bolgoson

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //2 toglogchiin eelj hadgalah huvsagch
 //Player 1 = 0, Player 2 = 1
 
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Players point 
 var scores = [0, 0];
@@ -9,8 +9,6 @@ var scores = [0, 0];
 //Player point now 
 var roundScore = 0;
 
-//Dice point random 1 - 6 hurtel 
-var dice = Math.ceil(Math.random() * 6);
 
 
 //Togloom ehleh stage
@@ -21,12 +19,35 @@ document.getElementById('current-1').textContent = 0;
 
 document.querySelector('.dice').style.display = "none";
 
+//  Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", shooShid);
 
 function shooShid() {
+    //1-6 доторх санамсаргүй нэг тоо
     var diceNumber = Math.ceil(Math.random() * 6);
 
     document.querySelector('.dice').style.display = "block";
     document.querySelector('.dice').src = 'dice-' + diceNumber + ".png";
-       
+    
+    if (diceNumber != 1) {
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+
+    } else {
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+
+
+        if (activePlayer === 0){
+            activePlayer = 1;
+        } else {
+            activePlayer = 0;
+        }
+
+        document.querySelector('.player-0-panel').classList.toggle("active");
+        document.querySelector('.player-1-panel').classList.toggle("active");
+
+        // Шоог алга болгоно
+        document.querySelector('.dice').style.display= "none";
+    }
 }
